03ef7712a4a86476194a658c06b901105a3b28f2
Kevin Lee <kevlee521@gmail.com>
2017-11-07 08:16:35 -0800

    first commit

--- dev/js/reducers/reducer-users.js
+++ dev/js/reducers/reducer-users.js
@@ -1,10 +1,5 @@
-/*
- * The users reducer will always return an array of users no matter what
- * You need to return something, so if there are no users then just return an empty array
- * */
-
 export default function () {
-    return [
+  return [
         {
             id: 1,
             first: "Bucky",
